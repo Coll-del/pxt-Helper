168a25f0e38ad5d45ce8088809c12a2c1e6913a4
Coll-del <robert.j.carr5@gmail.com>
2024-11-28 14:53:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+
+//% color=#D83B01
+namespace Cool {
+    /**
+     * Turns by an angle between 0 and 180
+     */
+    //% block="turn $angle"
+    //% angle.shadow="protractorPicker"
+    export function turn(angle: number) {
+
+    }
+
+    //% block="show color $color"
+    //% color.shadow="colorNumberPicker"
+    export function showColor(color: number) {
+
+    }
+
+    //% block
+    export function move(steps: WindowLocalStorage) {
+    }
+
